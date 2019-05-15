b0396e56790d0149182b4442f0f50803953350d1
William Wheeler <wgwheelerjr@icloud.com>
2019-05-15 17:27:29 -0500

    hello world; initial commit

---
+++ src/main/java/com/norsche/MinimaListApplication.java
@@ -0,0 +1,13 @@
+package com.norsche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinimaListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinimaListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/norsche/MinimaListApplicationTests.java
@@ -0,0 +1,16 @@
+package com.norsche;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MinimaListApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
