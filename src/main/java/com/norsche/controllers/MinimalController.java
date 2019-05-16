cdb4f202421e8bb4fb95d48ae868e6fab4e21ee0
William Wheeler <wgwheelerjr@icloud.com>
2019-05-16 13:24:58 -0500

    added add.html (2nd view) and added ability to add anime to a non-persistent list

--- src/main/java/com/norsche/controllers/MinimalController.java
+++ src/main/java/com/norsche/controllers/MinimalController.java
@@ -3,15 +3,38 @@ package com.norsche.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping(value = "anime")
 public class MinimalController {
 
+    static ArrayList<String> anime_alist = new ArrayList<>();
+
     // Request path: /anime
     @RequestMapping(value = "")
     public String index(Model model) {
+
+        model.addAttribute("anime_alist", anime_alist);
         model.addAttribute("title", "My Anime Minima-List");
+
         return "anime/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddAnimeForm(Model model){
+         model.addAttribute("title", "Add Anime");
+        return "anime/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddAnimeForm(@RequestParam String animeName){
+        anime_alist.add(animeName);
+
+        //Redirect to /anime
+        return "redirect:";
     }
 }
